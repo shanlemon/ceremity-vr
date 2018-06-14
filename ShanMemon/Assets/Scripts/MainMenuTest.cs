f6d9a342689b83f3b463dfc950f6746c56cb9f96
Shan Memon <shankmemon@gmail.com>
2018-06-14 17:48:26 -0500

    Added my personal sandbox project

---
+++ ShanMemon/Assets/Scripts/MainMenuTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuTest : MonoBehaviour {
+
+	[SerializeField]
+	private Text text;
+
+	public void ChangeScene(int sceneIndex) {
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void QuitGame() {
+		Debug.Log("Quit Game");
+		Application.Quit();
+	}
+
+	public void UpdateTextFromSlider(float value) {
+		text.text = value.ToString("N2");
+	}
+
+
+}
+
